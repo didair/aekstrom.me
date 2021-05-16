0f239cb4fbe8ccda6313fe2a7a96eb0f4eb288b6
Andreas Ekström <didair@msn.com>
2021-05-16 21:53:01 +0200

    WIP renders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 // Containers
 import App from 'containers/App';
+import Renders from 'containers/Renders'
 
 // Load external CSS deps
 import '@dracula/dracula-ui/styles/dracula-ui.css';
@@ -11,10 +13,15 @@ import '@dracula/dracula-ui/styles/dracula-ui.css';
 // App specific CSS
 import 'styles/index.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
 	<React.StrictMode>
-		<header>header</header>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<header>header</header>
+			<App />
+			<Renders />
+		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
